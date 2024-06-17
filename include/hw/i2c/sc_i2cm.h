68deec6dabef44d2e206ccce103f38f0bedb5e91
Shin-ya Koga <koga@sj8.so-net.ne.jp>
2024-06-18 07:14:04 +0900

    change 'sc_obc_i2c' to 'sc_i2cm'
    'sc obc i2c' actually meant 'Space Cubics' I2C Master Controller', so renamed it.

---
+++ include/hw/i2c/sc_i2cm.h
@@ -0,0 +1,14 @@
+/*
+ * Space Cubics OBC FPGA's I2C Controller type difinition
+ */
+
+#ifndef SC_I2CM_H
+#define SC_I2CM_H
+
+#ifndef QEMU_I2C_H
+#include "hw/i2c/i2c.h"
+#endif
+
+#define TYPE_SC_I2CM  "sc-i2cm"
+
+#endif /* SC_I2CM_H */
